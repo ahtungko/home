13a006b9840f16ec3aa810e74b41d43ada48af24
xka <itko1993@gmail.com>
2025-02-04 18:17:46 +0800

    Testing

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bell } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bell, Rss } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -71,6 +71,7 @@ const siteIcon = {
   Fire,
   LaptopCode,
   Bell,
+  Rss,
 };
 
 // 链接跳转
