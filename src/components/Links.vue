3e87a1d99d242c4fa714a91e27e11261951876f7
xka <itko1993@gmail.com>
2025-02-08 16:26:49 +0800

    change icon and remove fq

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,8 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bell, Rss, Brain} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bell, Rss} from "@vicons/fa"; // 注意使用正确的类别
+import { BrainCircuit20Regular } from '@vicons/fluent'
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -72,7 +73,7 @@ const siteIcon = {
   LaptopCode,
   Bell,
   Rss,
-  Brain,
+  BrainCircuit20Regular,
 };
 
 // 链接跳转
